1469280c0c05b2e2eba10604f9e5e9a32fdea95d
Larry Li <86larryli@gmail.com>
2022-03-25 19:53:08 -0400

    Updated First Draft Schema (Milestone 1)

--- db.js
+++ db.js
@@ -1,34 +1,73 @@
 const mongoose = require('mongoose'),
 	URLSlugs = require('mongoose-url-slugs'),
-  passportLocalMongoose = require('passport-local-mongoose');
+	passportLocalMongoose = require('passport-local-mongoose');
 
 
 const User = new mongoose.Schema({
-  // username, password
-  lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }]
+	// username, password
 });
 
-const Item = new mongoose.Schema({
-	name: {type: String, required: true},
-	quantity: {type: Number, min: 1, required: true},
-	checked: {type: Boolean, default: false, required: true}
+const Country = new mongoose.Schema({
+	countryname: { type: String, required: true }
+}, {
+	_id: true
+});
+
+const City = new mongoose.Schema({
+	countryid: { type: String, required: true },
+	cityname: { type: String, required: true }
+}, {
+	_id: true
+});
+
+const Airport = new mongoose.Schema({
+	cityid: { type: String, required: true },
+	airportcode: { type: String, required: true }
+}, {
+	_id: true
+});
+
+const Airline = new mongoose.Schema({
+	countryid: { type: String, required: true },
+	airlinename: { type: String, required: true }
+}, {
+	_id: true
+});
+
+const Schedule = new mongoose.Schema({
+	airlineid: { type: String, required: true },
+	flightno: { type: String, required: true },
+	flightdate: { type: Date, required: true },
+	flightschedule: { type: String, required: true },
+	flightplan: { type: String, required: true },
+	depcityid: { type: String, required: true },
+	arrcityid: { type: String, required: true },
+	stopno: { type: Number, required: true },
+	stops: { type: Object, required: true }
 }, {
 	_id: true
 });
 
 
-const List = new mongoose.Schema({
-  user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-  name: {type: String, required: true},
-	createdAt: {type: Date, required: true},
-	items: [Item]
+const WatchList = new mongoose.Schema({
+	userid: { type: String, required: true },
+	flightno: { type: String, required: true },
+	startdate: { type: Date, required: true },
+	enddate: { type: Date, required: true },
+	watchtype: { type: Number, required: true }
+}, {
+	_id: true
 });
 
 
 User.plugin(passportLocalMongoose);
-List.plugin(URLSlugs('name'));
 
 mongoose.model('User', User);
-mongoose.model('List', List);
-mongoose.model('Item', Item);
-mongoose.connect('mongodb://localhost/grocerydb');
+mongoose.model('Country', Country);
+mongoose.model('City', City);
+mongoose.model('Airport', Airport);
+mongoose.model('Airline', Airline);
+mongoose.model('Schedule', Schedule);
+mongoose.model('WatchList', WatchList);
+
+mongoose.connect('mongodb://localhost/takemehome-dev');
