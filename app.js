9bfcb9aeacda8efcf9b8b7b85892f48e14bf059f
Larry Li <86larryli@gmail.com>
2022-04-06 17:58:53 -0400

    Removed authentications

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 
 require('./db');
-require('./auth');
+// require('./auth');
 
 const passport = require('passport');
 const express = require('express');
 const path = require('path');
 
-const routes = require('./routes/index');
-const list = require('./routes/list');
-const listItem = require('./routes/list-item');
+const index = require('./routes/index');
+// const list = require('./routes/list');
+// const listItem = require('./routes/list-item');
 
 const app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // enable sessions
 const session = require('express-session');
 const sessionOptions = {
-    secret: 'secret cookie thang (store this elsewhere!)',
-    resave: true,
-      saveUninitialized: true
+  secret: 'secret cookie thang (store this elsewhere!)',
+  resave: true,
+  saveUninitialized: true
 };
 app.use(session(sessionOptions));
 
@@ -38,8 +37,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', routes);
-app.use('/list', list);
-app.use('/list-item', listItem);
+app.use('/', index);
+// app.use('/list', list);
+// app.use('/list-item', listItem);
 
 app.listen(3000);
