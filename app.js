b244d1c86976c68ae735ab316fc9fc2f43768fbb
Larry Li <86larryli@gmail.com>
2022-04-06 22:32:00 -0400

    Fixed CORS error

--- app.js
+++ app.js
@@ -30,6 +30,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", '3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
 // make user data available to all templates
 app.use((req, res, next) => {
   res.locals.user = req.user;
